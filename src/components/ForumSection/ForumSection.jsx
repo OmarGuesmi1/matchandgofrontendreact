4855d9608ee7fe22d480997119f92d7a3b5a54ed
omarguesmi <guesmiomarog@gmail.com>
2025-07-17 02:36:43 +0100

    add the condidaturesection and the forumsection component

---
+++ src/components/ForumSection/ForumSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ForumSection = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default ForumSection
--- src/components/JobSection/JobSection.jsx
+++ src/components/JobSection/JobSection.jsx
@@ -4,21 +4,19 @@ import './JobSection.css';
 
 const JobSection = () => {
   return (
-    <div className="job-section">
-      <h1 className="job-image-title">Find a job</h1>
-      
-      <div className="job-section-image-container">
-        <img src={assets.jobsection} alt="Team working" className="job-image" />
+  <div className="job-section-image-container">
+  <img src={assets.jobsection} alt="Team working" className="job-image" />
 
-        <div className="job-text-overlay">
-          <h1>Find a Place Where You Belong</h1>
-          <p>Explore authentic teams, values, and daily life. Go beyond job titles to find real fit.</p>
-          <p>Every company has a story. Discover their culture before applying.</p>
-          <p><strong>MATCH&GO</strong> helps you connect with companies that match your goals and values — faster, smarter, and more personally.</p>
-          <button className="find-job-button">Find Job</button>
-        </div>
-      </div>
-    </div>
+
+  <div className="job-text-overlay">
+    <h1>Find a Place Where You Belong</h1>
+    <p>Explore authentic teams, values, and daily life. Go beyond job titles to find real fit.</p>
+    <p>Every company has a story. Discover their culture before applying.</p>
+    <p><strong>MATCH&GO</strong> helps you connect with companies that match your goals and values — faster, smarter, and more personally.</p>
+    <button className="find-job-button">Find Job</button>
+  </div>
+</div>
+
   );
 };
 
